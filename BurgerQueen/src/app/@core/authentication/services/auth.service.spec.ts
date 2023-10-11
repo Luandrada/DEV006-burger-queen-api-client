955f813255c7bfed8974528f95ada663aa5a2511
LuanaYellow <luana@yellowpatito.com>
2023-10-11 14:46:36 -0300

    testeando systemUSer y localstorage al iniciar y cerrar sesion

--- BurgerQueen/src/app/@core/authentication/services/auth.service.spec.ts
+++ BurgerQueen/src/app/@core/authentication/services/auth.service.spec.ts
@@ -4,17 +4,38 @@ import { AuthService, Credentials } from './auth.service';
 import { LoginResponse } from '../../../shared/models/Login';
 import { of } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { LocalStorageService } from '../../services/local-storage.service';
 import { HttpClient } from '@angular/common/http';
+import { systemUser } from '../../interfaces';
 
 class MockLocalStorageService {
-  setStorage() {}
+  private storage: { [key: string]: any } = {};
+
+  setItem(key: string, value: any): void {
+    this.storage[key] = value;
+  }
+
+  getItem(key: string): any {
+    return this.storage[key] || null;
+  }
 }
 
 describe('AuthService', () => {
   let authService: AuthService;
   let httpTestingController: HttpTestingController;
   let httpClientSpy: { post: jasmine.Spy };
+  let localStorageService: MockLocalStorageService; 
+
+
+  const mockCredentials: Credentials = { email: 'test@test.com', password: 'password' };
+
+  const mockResponse :LoginResponse = {
+      user: {
+        id: 1,
+        role: 'user',
+        email: 'test@test.com',
+      },
+      accessToken: 'mockAccessToken',
+    };
 
   beforeEach(() => {
     httpClientSpy = jasmine.createSpyObj('HttpClient', ['post']);
@@ -23,27 +44,17 @@ describe('AuthService', () => {
       imports: [HttpClientTestingModule],
       providers: [
         AuthService,
-        { provide: LocalStorageService, useClass: MockLocalStorageService },
+        { provide: MockLocalStorageService, useClass: MockLocalStorageService },
         { provide: HttpClient, useValue: httpClientSpy },
       ],    });
     
     authService = TestBed.inject(AuthService);
     httpTestingController = TestBed.inject(HttpTestingController);
+    localStorageService = TestBed.inject(MockLocalStorageService);
   });
 
 
   it('send a POST request when calling login', () => {
-    const mockCredentials: Credentials = { email: 'test@test.com', password: 'password' };
-
-    const mockResponse :LoginResponse = {
-        user: {
-          id: 1,
-          role: 'user',
-          email: 'test@test.com',
-        },
-        accessToken: 'mockAccessToken',
-      };
-
     httpClientSpy.post.and.returnValue(of(mockResponse));
 
     authService.login(mockCredentials);
@@ -55,12 +66,38 @@ describe('AuthService', () => {
     });
   });
 
+  it('systemUSer$ is filled correctly', () => {
+    httpClientSpy.post.and.returnValue(of(mockResponse));
+    authService.login(mockCredentials);
+
+    authService.systemUser$.subscribe((user: systemUser) => {
+      expect(user.id).toBe('1');
+      expect(user.role).toBe('user');
+      expect(user.email).toBe('test@test.com');
+    });
+  });
+
+  it('localStorage is filled correctly', () => {
+    httpClientSpy.post.and.returnValue(of(mockResponse));
+    authService.login(mockCredentials);
+
+    expect(localStorageService.getItem('accessToken')).toBe('mockAccessToken');
+    expect(localStorageService.getItem('role')).toBe('user');
+    expect(localStorageService.getItem('idUser')).toBe('1');
+  });
+ 
   it('should clear systemUser$ when calling logout', () => {
     authService.logout();
-
     authService.systemUser$.subscribe((user) => {
       expect(user).toEqual({ id: '', accessToken: '', role: '', email: '' });
     });
+  });
+
+  it('should clear localStorage when calling logout', () => {
+    authService.logout();
+    expect(localStorageService.getItem('accessToken')).toBe(null);
+    expect(localStorageService.getItem('role')).toBe(null);
+    expect(localStorageService.getItem('idUser')).toBe(null);
   });
 
   afterEach(() => {

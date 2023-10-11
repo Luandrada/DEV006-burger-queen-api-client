976758a4f95a6e5dc4d2550f7c91dbf8792262f4
LuanaYellow <luana@yellowpatito.com>
2023-10-11 15:10:21 -0300

    saltos de linea y cambio de nombre en variable modal

--- BurgerQueen/src/app/features/waitress/components/modal/modal.component.ts
+++ BurgerQueen/src/app/features/waitress/components/modal/modal.component.ts
@@ -7,12 +7,12 @@ import { ProductItemList } from 'src/app/shared/models/Product';
   styleUrls: ['./modal.component.scss']
 })
 export class ModalComponent {
-  @Input() data: ProductItemList | null = null;
+  @Input() product: ProductItemList | null = null;
   @Output() acceptButton: EventEmitter<number> = new EventEmitter<number>();
   @Output() closeModal: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   handleAccept () {
-    this.acceptButton.emit(this.data?.product.id);
+    this.acceptButton.emit(this.product?.product.id);
     this.closeModal.emit();
   }
 }

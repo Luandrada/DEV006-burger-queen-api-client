3a327dcd655f4c8e15d306db8ebad235f8b1d962
LuanaYellow <luana@yellowpatito.com>
2023-10-12 11:10:14 -0300

    test componente sign in

--- BurgerQueen/src/app/@core/authentication/sign-in/sign-in.component.spec.ts
+++ BurgerQueen/src/app/@core/authentication/sign-in/sign-in.component.spec.ts
@@ -3,49 +3,70 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SignInComponent } from './sign-in.component';
 import { AuthService } from 'src/app/@core/authentication/services/auth.service';
-import { LocalStorageService } from '../../services/local-storage.service';
 import { of } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 describe('SignInComponent', () => {
   let component: SignInComponent;
   let fixture: ComponentFixture<SignInComponent>;
   let authService: AuthService;
-  let localStorageService: LocalStorageService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [SignInComponent],
       imports: [ReactiveFormsModule, RouterTestingModule],
-      providers: [
-        {
-          provide: AuthService,
-          useValue: {
-            sigIn: () => of({ accessToken: 'fakeToken', user: { role: 'mesera', id: 1 } }),
-          },
-        },
-        {
-          provide: LocalStorageService,
-          useValue: {
-            setStorage: () => {},
-          },
-        },
-      ],
+      providers: [AuthService],
     });
     fixture = TestBed.createComponent(SignInComponent);
     component = fixture.componentInstance;
     authService = TestBed.inject(AuthService);
-    localStorageService = TestBed.inject(LocalStorageService);
+    spyOn(authService, 'login').and.callFake(() => {
+      return;
+    });
+    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('should initialize the form', () => {
+  it(' initialize the form', () => {
     fixture.detectChanges();
     expect(component.formLogin).toBeTruthy();
     expect(component.formLogin.get('email')).toBeTruthy();
     expect(component.formLogin.get('password')).toBeTruthy();
+  });
+
+  it('should call signIn when the form is valid', () => {
+    spyOn(component, 'signIn');
+    component.formLogin.setValue({
+      email: 'test@example.com',
+      password: 'password',
+    });
+    component.signIn();
+    expect(component.signIn).toHaveBeenCalled();
+  });
+
+  it('should not call signIn when the form is invalid', () => {
+    spyOn(component, 'signIn');
+    component.formLogin.setValue({
+      email: 'test@example.com',
+      password: '', 
+    });
+    component.signIn();
+    expect(component.signIn).not.toHaveBeenCalled();
+  });
+
+  it('handle login errors', () => {
+    const errorResponse = new HttpErrorResponse({ error: 'Incorrect password' });
+    spyOn(authService, 'login').and.callFake(() => {
+      return of(errorResponse);
+    });
+
+    component.formLogin.setValue({
+      email: 'test@example.com',
+      password: 'password',
+    });
+
+    component.signIn();
+
+    expect(component.errorMessage).toBe('Credenciales Inválidas');
   });
 
 });
